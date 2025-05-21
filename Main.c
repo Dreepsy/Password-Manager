ef27cf9635c3414f9b5150d4e7276fc1e50800b1
Dreepsy <wesolowski.leo@gmail.com>
2025-05-21 12:49:09 +0200

    change existing password

--- Main.c
+++ Main.c
@@ -13,6 +13,7 @@ struct Password
 void showMainMenu();
 void viewPasswords();
 void addNewPassword();
+void changePassword();
 int countPasswords();
 
 // ---Global variable---
@@ -35,7 +36,8 @@ void showMainMenu()
         printf("\n=== Password Manager ===\n");
         printf("1. Add new password\n");
         printf("2. Show entries\n");
-        printf("3. Exit\n");
+        printf("3. Change Password\n");
+        printf("4. Exit\n");
         printf("Choose an option: ");
         scanf("%d", &choice);
 
@@ -47,7 +49,12 @@ void showMainMenu()
             case 2:
                 viewPasswords();
                 break;
+
             case 3:
+                changePassword();
+                break;
+
+            case 4:
                 printf("Exiting the program.\n");
                 return;
             default:
@@ -106,7 +113,7 @@ void viewPasswords()
     fclose(fptr);
 }
 
-// ---Count lines (passwords) in the file---
+// ---Count lines in the file---
 int countPasswords() 
 {
     FILE *fptr = fopen("Password-Manager.txt", "r");
@@ -126,4 +133,76 @@ int countPasswords()
 
     fclose(fptr);
     return count;
+}
+
+void changePassword()
+{
+    struct Password list[100];
+    int count = 0;
+
+    // Open the file for reading
+    FILE *fptr = fopen("Password-Manager.txt", "r");
+    if (fptr == NULL)
+    {
+        printf("No passwords to edit!\n");
+        return;
+    }
+
+    // Load all entries from the file into the array
+    char line[256];
+    while (fgets(line, sizeof(line), fptr))
+    {
+        if (sscanf(line, "%d. %99[^:]: %99[^\n]", &list[count].P_ID, list[count].Name, list[count].Password) == 3)
+        {
+            count++;
+        }
+    }
+    fclose(fptr);
+
+    // Display entries
+    printf("\n=== Password Entries ===\n");
+    for (int i = 0; i < count; i++)
+    {
+        printf("%d. %s: %s\n", list[i].P_ID, list[i].Name, list[i].Password);
+    }
+
+    // Ask user which ID to update
+    int targetID;
+    printf("\nEnter the ID of the password you would like to change: ");
+    scanf("%d", &targetID);
+
+    // Search for the entry with the matching ID
+    int found = 0;
+    for (int i = 0; i < count; i++)
+    {
+        if (list[i].P_ID == targetID)
+        {
+            printf("Enter new password for %s: ", list[i].Name);
+            scanf("%s", list[i].Password);  // directly store the new password
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found)
+    {
+        printf("No entry found with ID %d.\n", targetID);
+        return;
+    }
+
+    // Rewrite the file with updated data
+    fptr = fopen("Password-Manager.txt", "w");
+    if (fptr == NULL)
+    {
+        printf("Error opening file for writing.\n");
+        return;
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+        fprintf(fptr, "%d. %s: %s\n", list[i].P_ID, list[i].Name, list[i].Password);
+    }
+
+    fclose(fptr);
+    printf("Password updated successfully.\n");
 }
